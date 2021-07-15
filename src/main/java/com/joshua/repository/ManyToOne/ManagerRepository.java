622a935cbd21be2224e87afcb452115a08cd1213
mingJ7235 <joshuara7235@gmail.com>
2021-07-15 18:07:02 +0900

    ManagerRepository -> add method for find by Name

--- src/main/java/com/joshua/repository/ManyToOne/ManagerRepository.java
+++ src/main/java/com/joshua/repository/ManyToOne/ManagerRepository.java
@@ -3,5 +3,8 @@ package com.joshua.repository.ManyToOne;
 import com.joshua.domain.ManyToOne.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+    Optional<Manager> findByName (String managerName);
 }
