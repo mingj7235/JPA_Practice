94ad8d4b3758aa66b786352dcc914843a542a35a
mingJ7235 <joshuara7235@gmail.com>
2021-07-16 09:03:54 +0900

    JpaRepository Method naming : find + By + [property name (should be exact]

--- src/main/java/com/joshua/repository/ManyToOne/ManagerRepository.java
+++ src/main/java/com/joshua/repository/ManyToOne/ManagerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
-    Optional<Manager> findByName (String managerName);
+    Optional<Manager> findByManagerName (String managerName);
 }
